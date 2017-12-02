fb2b73e8477d3c3dd0ba2fa5ea4913565e2d82db
omkolhe <omkolhe026@gmail.com>
2017-12-03 03:37:44 +0530

    Added some files for HMU,FU,IDU

--- Project-2/vhdl_files/Flag_Unit.vhd
+++ Project-2/vhdl_files/Flag_Unit.vhd
@@ -4,11 +4,13 @@ use ieee.numeric_std.all;
 
 entity flag_unit is
   port
-    (op_code : in std_logic_vector(3 downto 0);
-    ari_bits : in std_logic_vector(1 downto 0);
-    carry_2 : in std_logic;
-    zero_2 : in std_logic;
+    (IR : in std_logic_vector(15 downto 0); --
+    --ari_bits : in std_logic_vector(1 downto 0);
+    c_2 : in std_logic;
+    z_2 : in std_logic;
+    wr_rf_in : in std_logic
     flush_bit_flag_unit : in std_logic;
+    p5_a3 : in std_logic_vector(2 downto 0);
     en_c2 : out std_logic;
     en_z2 : out std_logic;
     wr_rf :out std_logic);
@@ -16,78 +18,150 @@ end flag_unit;
 
 architecture behave of flag_unit is
   begin
-  process(op_code,ari_bits,carry_2,zero_2,flush_bit_flag_unit)
+  process(IR,c_2,z_2,flush_bit_flag_unit,wr_rf_in,p5_a3)
   begin
-    if(flush_bit_flag_unit = '1') then
-      en_c2 <= '0';
-      en_z2 <= '0';
-      wr_rf <= '0';
-    else
-      if(op_code = "0000" and ari_bits = "00") then -- ADD instruction flags are set
-        en_c2 <= '1';
-        en_z2 <= '1';
-        wr_rf <= '1';
-      elsif(op_code = "0010" and ari_bits = "00") then -- NDU instruction zero flag set
-        en_c2 <= '0';
-        en_z2 <= '1';
-        wr_rf <= '1';
-      elsif(op_code = "0001") then -- ADI instruction both flags set
-        en_c2 <= '1';
-        en_z2 <= '1';
-        wr_rf <= '1';
-      elsif(op_code = "0000" and ari_bits = "10") then -- ADC
-        if(carry_2 = '1') then
-          en_c2 <= '1';
-          en_z2 <= '1';
-          wr_rf <= '1';
-        else
-          en_c2 <= '0';
-          en_z2 <= '0';
-          wr_rf <= '0';
-        end if;
-      elsif(op_code = "0000" and ari_bits = "01") then -- ADZ
-        if(zero_2 = '1') then
-          en_c2 <= '1';
-          en_z2 <= '1';
-          wr_rf <= '1';
-        else
-          en_c2 <= '0';
-          en_z2 <= '0';
-          wr_rf <= '0';
-        end if;
-      elsif(op_code = "0010" and ari_bits = "10") then -- NDC
-        if(carry_2 = '1') then
-          en_c2 <= '0';
-          en_z2 <= '1';
-          wr_rf <= '1';
-        else
-          en_c2 <= '0';
-          en_z2 <= '0';
-          wr_rf <= '0';
-        end if;
-      elsif(op_code = "0010" and ari_bits = "01") then -- NDZ
-        if(zero_2 = '1') then
-          en_c2 <= '0';
-          en_z2 <= '1';
-          wr_rf <= '1';
-        else
-          en_c2 <= '0';
-          en_z2 <= '0';
-          wr_rf <= '0';
-        end if;
-      elsif(op_code = "0100") then -- LW
-        en_c2 <= '0';
-        en_z2 <= '1';
-        wr_rf <= '1';
-      elsif(op_code = "0011" or op_code = "0110" or op_code = "1000" or op_code = "1001") then
+    if( p5_a3 = "111") then --r7 harzard
         en_c2 <= '0';
         en_z2 <= '0';
-        wr_rf <= '1';
-      else
+    elsif(flush_bit_flag_unit = '1') then -- flush
         en_c2 <= '0';
         en_z2 <= '0';
         wr_rf <= '0';
-      end if;
+    elsif((IR(15 downto 12) = "0000") or (IR(15 downto 12) = "0001") or (IR(15 downto 12) = "0010") or (IR(15 downto 12) = "0100")) then
+        if(((IR(15 downto 12) = "0000") and (IR(1 downto 0) = "00")) or (IR(15 downto 12) = "0001")) then -- ADD or ADI
+            en_c2 <= '1';
+            en_z2 <= '1';
+            wr_rf <= wr_rf_in;
+        elsif((IR(15 downto 12) = "0000") and (IR(1 downto 0) = "01")) then-- ADZ
+            if(z_2 = '1') then
+                en_c2 <= '1';
+                en_z2 <= '1';
+                wr_rf <= wr_rf_in;
+            else
+                en_c2 <= '0';
+                en_z2 <= '0';
+                wr_rf <= '0';
+            end if;
+        elsif((IR(15 downto 12) = "0000") and (IR(1 downto 0) = "10")) then-- ADC
+            if(c_2 = '1') then
+                en_c2 <= '1';
+                en_z2 <= '1';
+                wr_rf <= wr_rf_in;
+            else
+                en_c2 <= '0';
+                en_z2 <= '0';
+                wr_rf <= '0';
+            end if;
+        elsif((IR(15 downto 12) = "0010") and (IR(1 downto 0) = "01")) then -- NDZ
+            if(z_2 = '1') then
+                en_c2 <= '0';
+                en_z2 <= '1';
+                wr_rf <= wr_rf_in;
+            else
+                en_c2 <= '0';
+                en_z2 <= '0';
+                wr_rf <= '0';
+            end if;
+        elsif((IR(15 downto 12) = "0000") and (IR(1 downto 0) = "10")) then -- NDC
+            if(c_2 = '1') then
+                en_c2 <= '0';
+                en_z2 <= '1';
+                wr_rf <= wr_rf_in;
+            else
+                en_c2 <= '0';
+                en_z2 <= '0';
+                wr_rf <= '0';
+            end if;
+        elsif((IR(15 downto 12) = "0010") and (IR(1 downto 0) = "00")) then-- NDU
+            en_c2 <= '0';
+            en_z2 <= '1';
+            wr_rf <= wr_rf_in;
+        elsif(IR(15 downto 0) = "0100") then -- LW
+            en_c2 <= '0';
+            en_z2 <= '1';
+            wr_rf <= wr_rf_in;
+        else
+            en_c2 <= '0';
+            en_z2 <= '0';
+            wr_rf <= wr_rf_in;
+        end if;
+    else
+        en_c2 <= '0';
+        en_z2 <= '0';
+        wr_rf <= wr_rf_in;
     end if;
+
+
+    -- if(flush_bit_flag_unit = '1') then
+    --   en_c2 <= '0';
+    --   en_z2 <= '0';
+    --   wr_rf <= '0';
+    -- else
+    --   if(op_code = "0000" and ari_bits = "00") then -- ADD instruction flags are set
+    --     en_c2 <= '1';
+    --     en_z2 <= '1';
+    --     wr_rf <= '1';
+    --   elsif(op_code = "0010" and ari_bits = "00") then -- NDU instruction zero flag set
+    --     en_c2 <= '0';
+    --     en_z2 <= '1';
+    --     wr_rf <= '1';
+    --   elsif(op_code = "0001") then -- ADI instruction both flags set
+    --     en_c2 <= '1';
+    --     en_z2 <= '1';
+    --     wr_rf <= '1';
+    --   elsif(op_code = "0000" and ari_bits = "10") then -- ADC
+    --     if(carry_2 = '1') then
+    --       en_c2 <= '1';
+    --       en_z2 <= '1';
+    --       wr_rf <= '1';
+    --     else
+    --       en_c2 <= '0';
+    --       en_z2 <= '0';
+    --       wr_rf <= '0';
+    --     end if;
+    --   elsif(op_code = "0000" and ari_bits = "01") then -- ADZ
+    --     if(zero_2 = '1') then
+    --       en_c2 <= '1';
+    --       en_z2 <= '1';
+    --       wr_rf <= '1';
+    --     else
+    --       en_c2 <= '0';
+    --       en_z2 <= '0';
+    --       wr_rf <= '0';
+    --     end if;
+    --   elsif(op_code = "0010" and ari_bits = "10") then -- NDC
+    --     if(carry_2 = '1') then
+    --       en_c2 <= '0';
+    --       en_z2 <= '1';
+    --       wr_rf <= '1';
+    --     else
+    --       en_c2 <= '0';
+    --       en_z2 <= '0';
+    --       wr_rf <= '0';
+    --     end if;
+    --   elsif(op_code = "0010" and ari_bits = "01") then -- NDZ
+    --     if(zero_2 = '1') then
+    --       en_c2 <= '0';
+    --       en_z2 <= '1';
+    --       wr_rf <= '1';
+    --     else
+    --       en_c2 <= '0';
+    --       en_z2 <= '0';
+    --       wr_rf <= '0';
+    --     end if;
+    --   elsif(op_code = "0100") then -- LW
+    --     en_c2 <= '0';
+    --     en_z2 <= '1';
+    --     wr_rf <= '1';
+    --   elsif(op_code = "0011" or op_code = "0110" or op_code = "1000" or op_code = "1001") then
+    --     en_c2 <= '0';
+    --     en_z2 <= '0';
+    --     wr_rf <= '1';
+    --   else
+    --     en_c2 <= '0';
+    --     en_z2 <= '0';
+    --     wr_rf <= '0';
+    --   end if;
+    -- end if;
 	end process;
 end behave;
--- Project-2/vhdl_files/PC_Control_Block.vhd
+++ Project-2/vhdl_files/PC_Control_Block.vhd
@@ -4,31 +4,37 @@ use ieee.numeric_std.all;
 
 entity PC_Control_Block is
   port
-    (op_code : in std_logic_vector(3 downto 0);
-     reset : in std_logic;
-     PC_1 : in std_logic_vector(15 downto 0);
+    (IR : in std_logic_vector(15 downto 0);
+     flush_bit_pipe2 : in std_logic;
+     add1 : in std_logic_vector(15 downto 0);
      PC_Imm : in std_logic_vector(15 downto 0);
      comp2_beq : in std_logic;
      D2_in : in std_logic_vector(15 downto 0);
-     PC_out : out std_logic_vector(15 downto 0));
+     PCL_out : out std_logic_vector(15 downto 0);
+     flush_2_pipe : out std_logic);
 end PC_Control_Block;
 
 architecture behave of PC_Control_Block is
   begin
-    process(op_code,reset,PC_1,PC_Imm,comp2_beq,D2_in)
+    process(IR,add1,PC_Imm,comp2_beq,D2_in,flush_bit_pipe2)
     begin
-      if(reset = '1') then
-        PC_out <= PC_1;
-      else
-        if(op_code = "1100" and comp2_beq = '1') then
-          PC_out <= PC_Imm;
-        elsif(op_code = "1000") then
-          PC_out <= PC_Imm;
-        elsif(op_code = "1001") then
-          PC_out <= D2_in;
+        if(flush_bit_pipe2 = '1') then
+            PCL_out <= add1;
+            flush_2_pipe <= '0';
         else
-          PC_out <= PC_1;
+            if(IR(15 downto 12) = "1000") then
+                PCL_out <= PC_Imm;
+                flush_2_pipe <= '1';
+            elsif(IR(15 downto 12) = "1001") then
+                PCL_out <= D2_in;
+                flush_2_pipe <= '1';
+            elsif(IR(15 downto 12) = "1100" and comp2_beq = '1') then
+                PCL_out <= PC_Imm;
+                flush_2_pipe <= '1';
+            else
+                PCL_out <= add1;
+                flush_2_pipe <= '0';
+            end if;
         end if;
-      end if;
     end process;
 end behave;
