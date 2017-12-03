dc60425897373cce1b169db0e15bd8706d1060cb
SarthakNijhawan <sarthakn69@gmail.com>
2017-12-03 16:28:49 +0530

    counter.vhd changed

--- Project-2/vhdl_files/counter.vhd
+++ Project-2/vhdl_files/counter.vhd
@@ -1,30 +1,40 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
---library work;
---use work.basic.all;
+entity counter is
+	port(	clk,reset,enable: in std_logic;
+		overflow: out std_logic;
+		A : out std_logic_vector(2 downto 0));
+end entity;
 
-entity nCounter is
-	generic (n : integer );
-	port(
-		rst, clk : in std_logic;
-		count : out std_logic_vector(n downto 0)
-		);
-end nCounter;
-
-architecture behave of nCounter is
-	begin
-	process (en_either, rst)
-	begin
-	  if rst = '1' then
-	    count <= (others => '0');
-	  elsif rising_edge(en_either) then
-	    if en_up = '1' then
-	      count <= count + 1;
-	    else
-	      count <= count - 1;
-	    end if;
-	  end if;
+architecture behave of counter is
+	signal copy_A: std_logic_vector(2 downto 0);
+begin
+	process(clk)
+		variable temp: std_logic_vector(2 downto 0):="000" ;
+		begin
+			if( clk'event and clk = '1') then
+				if(reset = '0') then
+				    if enable = '1' then
+				      temp := temp + "001";
+				    end if;
+				else temp := "000";
+				end if;
+			end if;
+			copy_A <= temp;
+			A <= temp;
 	end process;
-end behave;
+	
+	process(copy_A)
+		begin
+			if(copy_A = "000" and reset = '0') then
+				overflow <= '1';
+			else
+				overflow <= '0';
+			end if;
+		end process;
+end behave;	
+	
